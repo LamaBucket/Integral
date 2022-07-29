a701b71e6d3e8d4c569faa36384d88aa7dac0c27
Gleb Bannyy <gleb.bny@gmail.com>
2022-07-29 15:58:25 +0300

    Create an ApiCallState Extension Method to format its data to File Log

---
+++ Integral.RestApi/SystemExtensions.cs
@@ -0,0 +1,36 @@
+﻿using Integral.RestApi.Models;
+using System.Text;
+
+namespace Integral.RestApi
+{
+    public static class SystemExtensions
+    {
+        public static string FormatToFileLog(this ApiCallState state, Exception? exception)
+        {
+            StringBuilder sb = new();
+
+            sb.Append($"User:{state.Username},");
+            sb.Append($"Role:{state.UserRole},");
+            sb.Append($"Controller:{state.ApiControllerType.Name},");
+            sb.Append($"ControllerMethod:{state.ApiControllerMethodName},");
+            sb.Append($"Success:{state.Success}");
+
+            if (!state.Success)
+            {
+                if (exception is not null)
+                {
+                    sb.Append($", DueTo:{exception.Message}.");
+                }
+                else
+                {
+                    sb.Append(", DueTo:UnknownError");
+                }
+               
+            }
+
+            sb.AppendLine();
+
+            return sb.ToString();
+        }
+    }
+}
