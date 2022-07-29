ebbc8eec7a8721edfbaab35aefe8915efd3c68d4
Gleb Bannyy <gleb.bny@gmail.com>
2022-07-29 16:47:28 +0300

    Create ApiCallInfo Extend ILogger so that it can work ApiCallInfo

---
+++ Integral.RestApi/Models/ApiCallInfo.cs
@@ -0,0 +1,36 @@
+﻿using Integral.Domain.Models.Enums;
+using System.Text;
+
+namespace Integral.RestApi.Models
+{
+    public class ApiCallInfo
+    {
+        public ApiCallInfo(string? username, Role? userRole, string controllerName, string targetControllerMethod)
+        {
+            Username = username;
+            UserRole = userRole;
+            ControllerName = controllerName;
+            TargetControllerMethod = targetControllerMethod;
+        }
+
+        public string? Username { get; set; }
+        
+        public Role? UserRole { get; set; }
+
+        public string ControllerName { get; set; }
+
+        public string TargetControllerMethod { get; set; }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new();
+
+            sb.Append($"User:{Username},");
+            sb.Append($"Role:{UserRole},");
+            sb.Append($"Controller:{ControllerName},");
+            sb.Append($"ControllerMethod:{TargetControllerMethod};");
+            
+            return sb.ToString();
+        }
+    }
+}
--- Integral.RestApi/SystemExtensions.cs
+++ Integral.RestApi/SystemExtensions.cs
@@ -5,6 +5,14 @@ namespace Integral.RestApi
 {
     public static class SystemExtensions
     {
-        
+        public static void LogError(this ILogger logger, ApiCallInfo info, Exception ex)
+        {
+            logger.LogError(ex, message: info.ToString());
+        }
+
+        public static void LogInformation(this ILogger logger, ApiCallInfo info)
+        {
+            logger.LogInformation(info.ToString());
+        }
     }
 }
