256405b2366ab00cb8aa48501062be779447ef7a
Gleb Bannyy <gleb.bny@gmail.com>
2022-08-22 13:49:29 +0300

    Update Equals to compare on Id

--- Integral.Domain/Models/DomainObject.cs
+++ Integral.Domain/Models/DomainObject.cs
@@ -3,5 +3,30 @@
     public class DomainObject
     {
         public int Id { get; set; }
+
+        public override bool Equals(object? obj)
+        {
+            if(obj is DomainObject dObj)
+            {
+                return Id == dObj.Id;
+            }
+
+            return base.Equals(obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return Id.GetHashCode();
+        }
+
+        public static bool operator ==(DomainObject? left, DomainObject? right)
+        {
+            return left?.Id == right?.Id;
+        }
+
+        public static bool operator !=(DomainObject? left, DomainObject? right)
+        {
+            return left?.Id != right?.Id;
+        }
     }
 }
