bd4b40a6085067701b74a3fb629eca5c1b31ac61
Gleb Bannyy <gleb.bny@gmail.com>
2022-08-23 11:06:43 +0300

    Fix Error With Json Serialization object cycle

--- Integral.RestApi/Program.cs
+++ Integral.RestApi/Program.cs
@@ -5,12 +5,13 @@ using Integral.EntityFramework.Services;
 using Integral.RestApi.Middlewares;
 using Integral.RestApi.Services;
 using Microsoft.AspNetCore.Authentication.Cookies;
+using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers().AddJsonOptions(x => x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
