f4fc8087fbfe8bc59bbff534c4085685ca38a505
Gleb Bannyy <gleb.bny@gmail.com>
2022-08-23 21:20:38 +0300

    Update User Role Equality Comparer

--- Integral.Domain/Models/UserRole.cs
+++ Integral.Domain/Models/UserRole.cs
@@ -7,5 +7,27 @@ namespace Integral.Domain.Models
         public int UserId { get; set; }
 
         public Role Role { get; set; }
+
+        public override bool Equals(object? obj)
+        {
+            if(obj is UserRole role)
+                return role.UserId == UserId && role.Role == Role;
+            return base.Equals(obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(UserId.GetHashCode(), Role.GetHashCode());
+        }
+
+        public static bool operator  ==(UserRole left, UserRole right)
+        {
+            return left.UserId == right.UserId && left.Role == right.Role;
+        }
+
+        public static bool operator !=(UserRole left, UserRole right)
+        {
+            return left.UserId != right.UserId || left.Role != right.Role;
+        }
     }
 }
