a9bb00cec1b4f43881fe13fe101b75be55c56e64
Gleb Bannyy <gleb.bny@gmail.com>
2022-08-24 19:52:10 +0300

    Create DataManipulation Model

---
+++ Integral.Domain/Services/IDataManipulateService.cs
@@ -0,0 +1,16 @@
+﻿using Integral.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Integral.Domain.Services
+{
+    public interface IDataManipulateService<T> where T : DomainObject
+    {
+        Task<DataLoadResult<T>> Load(IEnumerable<T> items);
+
+        Task<IEnumerable<T>?> Extract(IEnumerable<T> items);
+    }
+}
